ac9457d3703c78d1ac882bc4f4597bce76580a71
QuL <qvlywk@gmail.com>
2025-05-26 17:48:48 +0800

    use _ instead of i in for-loops; add newDeck()

--- deck.go
+++ deck.go
@@ -4,6 +4,21 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"One", "Two", "Three", "Four"}
+
+	// 不需要使用的index 用 _ 代替
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // receiver func
 func (d deck) print() {
 	for i, card := range d {

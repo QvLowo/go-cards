73d562b4cc35d211c1228a61c617bc6c211aa4f3
QuL <qvlywk@gmail.com>
2025-05-27 16:33:19 +0800

    join a Slice of Strings

--- deck.go
+++ deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type deck []string
 
@@ -28,4 +31,9 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// deck轉成string拼接,並用逗號分隔
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
 }

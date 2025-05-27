ce1ac04a27b9be7cdc3b9ebb24997d4937468113
QuL <qvlywk@gmail.com>
2025-05-27 14:37:43 +0800

    Multiple Return Values

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.print()
+	// 因為都在同一個package,所以可以直接使用deal方法,不用額外import等等
+	hand, remainingCards := deal(cards, 5)
+	// hand & remainingCards的型態都是deck
+	hand.print()           // deal第一個回傳的值
+	remainingCards.print() // deal第二個回傳的值
 
 }
